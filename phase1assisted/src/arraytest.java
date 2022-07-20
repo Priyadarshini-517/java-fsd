04b0ac487177918657a5c413b4022b16ecb24fe0
priyadarshini <jaya517@sasi.ac.in>
2022-07-20 16:33:34 +0530

    changes

---
+++ phase1assisted/src/arraytest.java
@@ -0,0 +1,27 @@
+
+public class arraytest {
+	
+
+		public static void main(String[] args) {
+
+		int a[]= {46,86,23,76,89};
+		System.out.println("Elements of array a: ");
+		for(int i=0;i<a.length;i++) {
+		System.out.println(+a[i]);
+		}
+
+		int[][] b = {{1,2,3},
+		            {2, 4, 6,}, 
+		            {3, 6, 9} };
+		System.out.println("Elements of array b: ");
+		for (int i=0;i<3;i++) {
+			for (int j=0;j<3;j++) {
+				System.out.println(b[i][j]);
+			}
+		}
+		      
+		     
+		      }
+		}
+
+
