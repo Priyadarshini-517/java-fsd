819bcaf758344966e105b170f31082758c3d3ce8
priyadarshini <jaya517@sasi.ac.in>
2022-10-17 08:35:05 +0530

    employee leaves

---
+++ leaves/backend-leaves/src/main/java/com/model/Employee.java
@@ -0,0 +1,49 @@
+package com.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+    private String name;
+    private String email;
+    private int leaves;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getLeaves() {
+		return leaves;
+	}
+	public void setLeaves(int salary) {
+		this.leaves = salary;
+	}
+
+}
